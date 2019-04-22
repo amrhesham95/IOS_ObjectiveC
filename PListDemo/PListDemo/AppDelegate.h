ec8df43e6e09dffc4ab8ce649cb957a7a9b79ac7
JETS Mobile Lab - 5 <jetsmobilelab-5@JETSs-Mac-mini-2.local>
2019-04-22 11:02:51 +0200

    PListDemo

---
+++ PListDemo/PListDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PListDemo
+//
+//  Created by JETS Mobile Lab - 5 on 4/22/19.
+//  Copyright © 2019 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PListDemo/PListDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PListDemo
+//
+//  Created by JETS Mobile Lab - 5 on 4/22/19.
+//  Copyright © 2019 ios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
