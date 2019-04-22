ab667189ddf8c0c3f56238bfecde1dd63b9b5543
JETS Mobile Lab - 5 <jetsmobilelab-5@JETSs-Mac-mini-2.local>
2019-04-22 12:19:11 +0200

    PresentViewAndGestureDemo

---
+++ PresentViewAndGestureDemo/PresentViewAndGestureDemo/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  PresentViewAndGestureDemo
+//
+//  Created by JETS Mobile Lab - 5 on 4/22/19.
+//  Copyright © 2019 ios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
